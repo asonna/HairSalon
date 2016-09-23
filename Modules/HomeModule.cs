4aee3aba4bc615ea4fda3c433b8cf1ea02da00fd
Jonathan Buchner <websites@jonathanbuchner.com>
2016-09-23 00:02:57 -0700

    In progress edit one functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -28,6 +28,24 @@ namespace HairSalon
         List<Stylist> stylists = Stylist.GetAll();
         return View["index.cshtml", stylists];
       };
+      Get["/edit_stylist/{id}"] = parameters =>
+      {
+        Stylist editStylist = Stylist.Find(parameters.id);
+        return View["change_and_delete_stylist.cshtml", editStylist];
+      };
+      Patch["/update_stylist/{id}"] = parameters => {
+        Stylist updateStylist = Stylist.Find(parameters.id);
+        updateStylist.Update(Request.Form["new_stylist_name"]);
+        return View["index.cshtml"];
+      };
+      Get["/delete_stylist/{id}"] = parameters =>
+      {
+        Stylist removedStylist = Stylist.Find(parameters.id);
+        removedStylist.Delete();
+
+        List<Stylist> stylists = Stylist.GetAll();
+        return View["index.cshtml", stylists];
+      };
       Get["/stylist/{id}"] = parameters =>
       {
         Stylist currentStylist = Stylist.Find(parameters.id);
@@ -41,7 +59,6 @@ namespace HairSalon
       Post["/add_client"] = _ => {
         Client newClient = new Client (Request.Form["new_client"],Request.Form["stylist_id"]);
         newClient.Save();
-
 
         Stylist currentStylist = Stylist.Find(Request.Form["stylist_id"]);
         List<Client> clients = currentStylist.FindClients();
